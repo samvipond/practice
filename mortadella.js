80634a715fe82f6a8a57915ec0a55146778dda3f
Sam Vipond <sammy.potato@gmail.com>
2017-10-03 23:00:26 -0400

    fix morta six

--- mortadella.js
+++ mortadella.js
@@ -3,4 +3,3 @@
 // a good version of this file only has five lines. everything else is bugs
 // hi this is a nice fourth line
 // a nice little fifth line for extra practice
-// an unfortunate sixth line, plz remove
