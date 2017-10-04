4c70fb7c27f99dafae417bcd326a81e9b61b6031
Sam Vipond <sammy.potato@gmail.com>
2017-10-03 22:18:02 -0400

    bad morta

--- mortadella.js
+++ mortadella.js
@@ -1,3 +1,5 @@
 // this is created on local version of samvipond-patch-1
 // i'll bugger it up maybe and have to travel back in time to when it was good.
 // a good version of this file only has three lines. everything else is bugs
+
+// oh jeez i went and committed this but that was a mistake, i must undo this
