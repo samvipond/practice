b84044de15bef4ea03439ed342d7d5347df0c89e
Sam Vipond <sammy.potato@gmail.com>
2017-10-03 22:52:33 -0400

    fifth

--- mortadella.js
+++ mortadella.js
@@ -1,5 +1,5 @@
 // this is created on local version of samvipond-patch-1
 // i'll bugger it up maybe and have to travel back in time to when it was good.
-// a good version of this file only has four lines. everything else is bugs
+// a good version of this file only has five lines. everything else is bugs
 // hi this is a nice fourth line
-
+// a nice little fifth line for extra practice
